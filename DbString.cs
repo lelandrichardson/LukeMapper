7edfd848355917857b0fc94e8974c4a7611cde01
Leland Richardson <leland.m.richardson@gmail.com>
2012-07-22 01:07:05 -0500

    test commit
    test

---
+++ DbString.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Data;
+
+namespace LukeMapper
+{
+    /// <summary>
+    /// This class represents a SQL string, it can be used if you need to denote your parameter is a Char vs VarChar vs nVarChar vs nChar
+    /// </summary>
+    public sealed class DbString
+    {
+        /// <summary>
+        /// Create a new DbString
+        /// </summary>
+        public DbString() { Length = -1; }
+        /// <summary>
+        /// Ansi vs Unicode 
+        /// </summary>
+        public bool IsAnsi { get; set; }
+        /// <summary>
+        /// Fixed length 
+        /// </summary>
+        public bool IsFixedLength { get; set; }
+        /// <summary>
+        /// Length of the string -1 for max
+        /// </summary>
+        public int Length { get; set; }
+        /// <summary>
+        /// The value of the string
+        /// </summary>
+        public string Value { get; set; }
+        /// <summary>
+        /// Add the parameter to the command... internal use only
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="name"></param>
+        public void AddParameter(IDbCommand command, string name)
+        {
+            if (IsFixedLength && Length == -1)
+            {
+                throw new InvalidOperationException("If specifying IsFixedLength,  a Length must also be specified");
+            }
+            var param = command.CreateParameter();
+            param.ParameterName = name;
+            param.Value = (object)Value ?? DBNull.Value;
+            if (Length == -1 && Value != null && Value.Length <= 4000)
+            {
+                param.Size = 4000;
+            }
+            else
+            {
+                param.Size = Length;
+            }
+            param.DbType = IsAnsi ? (IsFixedLength ? DbType.AnsiStringFixedLength : DbType.AnsiString) : (IsFixedLength ? DbType.StringFixedLength : DbType.String);
+            command.Parameters.Add(param);
+        }
+    }
+}
